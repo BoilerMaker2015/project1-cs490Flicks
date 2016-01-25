410524f728a07e80f161afd80057c2e9bf54e0cf
Johnny <cui77@purdue.edu>
2016-01-24 21:58:12 -0500

    Update MoviesCollectionViewController.swift

--- Flicks/MovieViewer/MoviesCollectionViewController.swift
+++ Flicks/MovieViewer/MoviesCollectionViewController.swift
@@ -2,8 +2,8 @@
 //  MoviesCollectionViewController.swift
 //  MovieViewer
 //
-//  Created by Mac User on 16/1/23.
-//  Copyright © 2016年 Malik Browne. All rights reserved.
+//  Created by wentai,cui on 16/1/23.
+//  Copyright © 2016年 wentai,cui. All rights reserved.
 //
 
 import UIKit
